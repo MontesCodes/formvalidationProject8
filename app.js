66d609160475cbcbf901d1f90983a3993b81d480
MontesCodes <MontiesWebDev@Gmail.com>
2024-05-26 07:37:00 -0400

    updating HTML/JS time 14:00

---
+++ app.js
@@ -0,0 +1,7 @@
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+// target the form element
+const form = document.getElementById('myForm');
+const confirmPassword = document.getElementById('confirmPassword');
