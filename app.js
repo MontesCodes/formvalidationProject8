6500ded2014310dc0da6d453be3e2d6762ec21cf
MontesCodes <MontiesWebDev@Gmail.com>
2024-05-26 08:00:19 -0400

    updating JS time 22:48

--- app.js
+++ app.js
@@ -5,3 +5,41 @@ const password = document.getElementById('password');
 // target the form element
 const form = document.getElementById('myForm');
 const confirmPassword = document.getElementById('confirmPassword');
+
+// Event listener for the form
+const green = '#4CAF60';
+const red = '#F44346';
+
+function validateFirstName() {
+  if (checkIfEmpty(firstName)) return;
+
+  if (!checkIfOnlyLetters(firstName)) return;
+  return true;
+}
+
+function checkIfEmpty(field) {
+  if (isEmpty(field.value.trim())) {
+    setInvalid(field, `${field.name} cannot be empty`);
+    return true;
+  } else {
+    setValid(field);
+    return false;
+  }
+}
+
+function isEmpty(value) {
+  if (value === '') return true;
+  return false;
+}
+
+function setInvalid(field, message) {
+  field.className = 'invalid';
+  field.nextElementSibling.innerHTML = message;
+  field.nextElementSibling.style.color = red;
+}
+
+function setValid(field) {
+  field.className = 'valid';
+  field.nextElementSibling.innerHTML = '';
+  field.nextElementSibling.style.color = green;
+}
